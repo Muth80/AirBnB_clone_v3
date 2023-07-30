38784bae2fa3bfde1c4519b9769ce71d6c3577cd
David Muindi <muindidiego@gmail.com>
2023-07-30 05:39:22 -0700

    Tasks

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 # __init__.py
 
 from flask import Blueprint
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
+
 # index.py
 
 from api.v1.views import app_views
+from flask import jsonify, Response
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """Returns a JSON with the status 'OK'"""
-    return jsonify({"status": "OK"})
+    response_data = {"status": "OK"}
+    return jsonify(response_data), 200, {"Content-Type": "application/json"}
 
