67b76cbac0aa511854c7745df708532bd77276c0
David Muindi <muindidiego@gmail.com>
2023-07-31 04:24:58 -0700

    Tasks

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -18,3 +19,10 @@ if __name__ == "__main__":
     port = int(os.getenv("HBNB_API_PORT", "5000"))
     app.run(host=host, port=port, threaded=True)
 
+# Handler for 404 errors
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
